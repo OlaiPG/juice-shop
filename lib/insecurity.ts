20d0613db8767bf2ed6a77684bef69222f011d68
OlaiPG <olaipg46@educastur.es>
2024-12-05 20:47:44 +0200

    Update insecurity.ts

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -20,7 +20,8 @@ import * as utils from './utils'
 import * as z85 from 'z85'
 
 export const publicKey = fs ? fs.readFileSync('encryptionkeys/jwt.pub', 'utf8') : 'placeholder-public-key'
-const 
+const with open("/encryptionkeys/private.key","r") as key_file:
+    private_key = key_file.read()
 
 interface ResponseWithUser {
   status: string
